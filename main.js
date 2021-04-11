f1668ff13ab3ca24c14c0aadd23a140a0fb7f4b9
adhyuva <adhyuva8@gmail.com>
2021-04-11 11:47:43 -0400

    Changing the victory sentence again

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ function makeArray(){
     return elements
 }
 function validate(){
-    document.getElementById("hint").innerHTML = `You guesses it! The answer was  ${secert.title}`;
+    document.getElementById("hint").innerHTML = `You guessed it! The answer was  ${secert.title}, element ${secert.id.slice(2)}`;
 }
 let elements = makeArray();
 let randomIndex = Math.floor(Math.random() * 118);
 let randomElement = document.getElementById(elements[randomIndex]);
 let secert = randomElement;
-console.log(secert.title);
+console.log(secert.id.slice(2));
 secert.addEventListener('click', validate);
