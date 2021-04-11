a80b0f9d658fc1818ed92f8225b2b93bc7d66d4a
adhyuva <adhyuva8@gmail.com>
2021-04-11 11:58:56 -0400

    mergin conflicts ~

--- main.js
+++ main.js
@@ -9,21 +9,15 @@ function validate(){
     document.getElementById("hint").innerHTML = `You guessed it! The answer was  ${secert.title}, element ${secert.id.slice(2)}`;
 }
 
-let elements = makeArray();
-let randomIndex = Math.floor(Math.random() * 118);
-let randomElement = document.getElementById(elements[randomIndex]);
-let secert = randomElement;
-console.log(secert.id.slice(2));
-secert.addEventListener('click', validate);
-
 function playGame(){
+    document.getElementById("hint").innerHTML = "";
     let elements = makeArray();
     let randomIndex = Math.floor(Math.random() * 118);
     let randomElement = document.getElementById(elements[randomIndex]);
     let secert = randomElement;
-    console.log(secert.id.slice(2));
+    console.log(secert);
     secert.addEventListener('click', validate);
 }
 playGame();
 let playBtn = document.getElementById("play");
-playBtn.addEventListener("click", playGame);
+playBtn.addEventListener("click", playGame)
